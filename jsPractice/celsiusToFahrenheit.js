0d657c9917418a064450fe3d74d28e646da50d6f
vinita1512 <vinitajambhale2000@gmail.com>
2024-11-03 23:22:35 +0530

    celsiusToFahrenheit

---
+++ jsPractice/celsiusToFahrenheit.js
@@ -0,0 +1,7 @@
+const celsiusToFahrenheit = (celsius) => {
+  const fahrenheit = celsius * 1.8 + 32;
+  console.log(
+    `${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit`
+  );
+};
+celsiusToFahrenheit(10);
